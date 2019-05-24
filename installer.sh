da03af1b4b007e2a0d3b0b9614c1020daa1f4b19
Uniminin <42061747+Uniminin@users.noreply.github.com>
2019-05-24 22:28:00 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -75,9 +75,11 @@ sudo apt updatesudo apt install code
 
 ###############################################################################################
 
-# Gitkraken
+# Transmission
 
-sh software/install-gitkraken.sh
+sudo add-apt-repository ppa:transmissionbt/ppa
+sudo apt-get update
+sudo apt-get install transmission-gtk transmission-cli transmission-common transmission-daemon
 
 ###############################################################################################
 
@@ -109,33 +111,41 @@ sudo apt-get install qbittorrent
 
 ###############################################################################################
 
-# OBS
+# DropBox
 
-sh software/install-obs.sh
-
-###############################################################################################
-
-# Peek
-
-sh software/install-peek.sh
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -    [On 32-Bit]
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - [On 64-Bit]
+~/.dropbox-dist/dropboxd
 
 ###############################################################################################
 
-# Spice-Up
+# Emacs
 
-sh software/install-spice-up.sh
-
-###############################################################################################
-
-# System Back
-
-sh software/install-systemback.sh
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt update
+sudo apt install emacs25
 
 ###############################################################################################
 
-# Teamviewer
+# Nano
 
-sh software/install-teamviewer.sh
+sudo add-apt-repository ppa:n-muench/programs-ppa
+sudo apt-get update
+sudo apt-get install nano
+
+###############################################################################################
+
+# Aria2
+
+sudo apt-get install aria2
+
+###############################################################################################
+
+# uGet
+
+sudo add-apt-repository ppa:plushuang-tw/uget-stable
+sudo apt update
+sudo apt install uget
 
 #############################################################################################
 
@@ -148,21 +158,229 @@ sudo apt-get update
 
 #############################################################################################
 
-# Ubuntu Cleaner
+# XDM
 
-sh software/install-ubuntu-cleaner.sh
+sudo add-apt-repository ppa:noobslab/apps
+sudo apt-get update
+sudo apt-get install xdman
+
+###############################################################################################
+
+# Thunderbird
+
+sudo add-apt-repository ppa:ubuntu-mozilla-security/ppa
+sudo apt-get update
+sudo apt-get install thunderbird
 
 ###############################################################################################
 
-# Virtualbox
+# Brackets
 
-sh software/install-virtual-box.sh
+sudo add-apt-repository ppa:webupd8team/brackets
+sudo apt-get update
+sudo apt-get install brackets
 
 ###############################################################################################
+
+# Tracer
+
+sudo add-apt-repository ppa:oguzhaninan/stacer
+sudo apt-get update
+sudo apt-get install stacer
+
+###############################################################################################
+
+# Gimp
+
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt update
+sudo apt install gimp
+
+###############################################################################################
+
+# Krita
+
+sudo add-apt-repository ppa:kritalime/ppa
+sudo apt update
+sudo apt install krita
+
+###############################################################################################
+
+# Openshot
+
+sudo add-apt-repository ppa:openshot.developers/ppa
+sudo apt update
+sudo apt install openshot-qt
+
+###############################################################################################
+
+# LibreOffice
+
+sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt update
+sudo apt install libreoffice
+
+###############################################################################################
+
+# shutter
+
+sudo add-apt-repository -y ppa:shutter/ppa
+sudo apt update
+sudo apt install shutter
+sudo add-apt-repository ppa:smathot/cogscinl
+sudo apt-get update
+###############################################################################################
+
+echo "Installing Core Pakages..."
+# Improvements for terminal
+sudo apt-get install fish
+
+cp /etc/skel/.bashrc ~/
+sudo chmod 644 ~/.bashrc
+
+# Lib
+sudo apt-get install libappindicator1
+
+## Java 
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
+# VLC Player
+sudo apt-get install vlc -y
+
+## Suporte ao Git
+sudo apt-get install git-core -y
+
+## Instalação MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+sudo apt-get update -y
+sudo apt-get install mongodb-10gen -y
+
+## Instalação Curl
+sudo apt-get install curl -y
+
+# PHP e MYSQL 
+sudo apt-get install php5-cli -y
+sudo apt-get install mysql-server -y
+sudo apt-get install php5-mysql -y
+
+sudo ln -sf /lib/i386-linux-gnu/libudev.so.1 /lib/i386-linux-gnu/libudev.so.0
+sudo apt-get install libmysql-ruby libmysqlclient-dev
+
+# phpmyadmin
+cd
+cd php/
+wget https://github.com/luizpicolo/exemplos/raw/master/phpMyAdmin.tar.gz
+tar -vzxf phpMyAdmin.tar.gz
+rm phpMyAdmin.tar.gz
+
+## Sublime Text 
+sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
+sudo apt-get update -y
+sudo apt-get install sublime-text -y
+
+# Coona Packagers
+rm -Rf ~/.config/sublime-text-2/
+git clone https://github.com/luizpicolo/MySublimeText2SupportFiles.git ~/.config/sublime-text-2/
+
+# PPa Maneger
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
+sudo apt-get update -y
+sudo apt-get install y-ppa-manager -y
+
+# Ruby com RVM
+\curl -L https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
+source ~/.bashrc
+rvm install 2.0.0 --autolibs=packages
+rvm use 2.0.0
+
+# PostGres
+sudo apt-get install postgresql -y
+sudo apt-get install libpq-dev -y
+sudo apt-get install pgadmin3 -y
+# sudo -u postgres psql template1
+# \password postgres
+sudo /etc/init.d/postgresql restart
+sudo update-rc.d postgresql enable
+
+# NodeJS
+#
+# USE NVM
+#
+# mkdir -p ~/.nodes && cd ~/.nodes
+# curl -O http://nodejs.org/dist/v0.10.30/node-v0.10.30-linux-x64.tar.gz
+# tar -xzf node-v0.10.30-linux-x64.tar.gz
+# mv node-v0.10.30-linux-x64 0.10.30
+# ln -s 0.10.12 current
+# rm ~/.nodes/node-v0.10.30-linux-x64.tar.gz
+
+# cd
+
+# echo 'export PATH="~/.nodes/current/bin:$PATH"' >> .bashrc
+# source ~/.bashrc
+
+# Heroku
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Nautilus OpenTerminal
+sudo apt-get install nautilus-open-terminal 
+
+# Icones LibraryOffice
+cd /usr/share/libreoffice/share/config/
+sudo wget http://blog.goranrakic.com/archives/slike/images_flat.zip
+sudo mv images_human.zip images_human.zip.bkp
+sudo mv images_flat.zip images_human.zip
+
+# Remover Online Pesquisar Lentes 
+sudo apt-get autoremove unity-lens-shopping -y
+sudo apt-get autoremove unity-lens-music -y
+sudo apt-get autoremove unity-lens-photos -y
+sudo apt-get autoremove unity-lens-gwibber -y
+sudo apt-get autoremove unity-lens-video -y
+
+# Alacarte
+sudo apt-get install alacarte
+
+# Pin Note
+sudo add-apt-repository ppa:umang/indicator-stickynotes -y
+sudo apt-get update -y
+sudo apt-get install indicator-stickynotes -y
+
+# SyncDriver
+sudo add-apt-repository ppa:noobslab/pear-apps -y
+sudo apt-get update -y
+sudo apt-get install syncdrive -y
+
+# remover seahorse
+sudo apt-get remove seahorse
+
+#Even tough Ubuntu Make is available in the official repositories, 
+# it's recommended to use its PPA so you always have the latest release.
+# To add the PPA and install Ubuntu Make, use the following commands:
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
+sudo apt update
+sudo apt install ubuntu-make
+
+# Install PopCornTime Official
+cd /opt/
+sudo wget https://ci.popcorntime.sh/job/Popcorn-Time-Desktop/lastSuccessfulBuild/artifact/Release/Popcorn-Time-Linux64.tar.xz
+sudo tar -xJf Popcorn-Time-Linux64.tar.xz
+sudo rm -f Popcorn-Time-Linux64.tar.xz
+sudo mv linux64/ popcorntime/
+# Create Symbolic Link
+sudo ln -s /opt/popcorntime/Popcorn-Time /usr/bin/popcorntime
+sudo apt-get update
 
 echo "################################################################"
 echo "##################  Extra Softwares Installed  ##################"
 echo "################################################################"
+
+echo "Installing Core Softwares"
 
 #software from 'normal' repositories
 sudo apt-get install -y apt-transport-https openssl software-properties-common python-software-properties git curl openssh-server ca-certificates zlib1g zlib1g-dev libpcre3 libpcre3-dev build-essential libssl-dev
@@ -178,34 +396,30 @@ echo "################################################################"
 echo "##################    core software installed  #################"
 echo "################################################################"
 
+echo "Installing dependencies 2nd..."
+apt-get update
+## SOME UPDATES FOR GCP VPSES OR OTHER VPS PROVIDER
+sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev -y	 
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update
+apt-get install python3 python3-dev -y
+add-apt-repository ppa:ondrej/php -y
+add-apt-repository ppa:longsleep/golang-backports -y
+apt-get update
+apt install git curl python3-pip python3-mysqldb -y
+apt-get install python-dev libmysqlclient-dev nginx software-properties-common libssl-dev mysql-server -y
+pip3 install --upgrade pip
+pip3 install flask
 
-echo "Installing Productivity..."
+apt-get install php7.0 php7.0-mbstring php7.0-mcrypt php7.0-fpm php7.0-curl php7.0-mysql golang-go -y
 
-# Arc theme
+apt-get install composer -y
+apt-get install zip unzip php7.0-zip -y
+apt-get update
 
-sh theme/install-gtk-arc-theme.sh
+echo "Done installing dependencies!"
 
-#############################################################################################
 
-# Arc OSX icons
-
-sh theme/install-arc-osx-icons.sh
-
-#############################################################################################
-
-# Plank themes
-
-sh theme/plank-themes.sh
-
-###############################################################################################
-
-# Cursor theme
-
-apt-get install -y breeze-cursor-theme
-
-echo "################################################################"
-echo "###################  themes/icons installed  ###################"
-echo "################################################################"
 
 echo "################################################################"
 echo "####################    T H E   E N D    #######################"
